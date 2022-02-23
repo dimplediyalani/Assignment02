a9ee125ecb3389c2d7b1fc61b3a2d29a91de8faf
dimplediyalani <dimplejd2011@gmail.com>
2022-02-23 18:15:04 -0500

    Initial Commit

---
+++ Assignment02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment02
+//
+//  Created by user214042 on 2/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
